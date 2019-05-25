3275d3fbbc58c99816e24f27756bf1f77650dfcd
David Duggins <weatheredwatcher@gmail.com>
2019-05-25 01:04:05 -0400

    added base structure to the cli app

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+const minimist = require('minimist')
+const https = require('https')
 module.exports = () => {
-    console.log('Welcome to the Name Generator')
+    const args = minimist(process.argv.slice(2))
+
+    const options = new URL('https://api.genr8rs.com/Generator/Gaming/Rpg/NameGenerator?genr8rsUserId=1558757955.64345ce8c2439d155&_sGenre=fantasy&_sRace=' + args.race + '&_sGender=' + args.gender);
+
+    const req = https.get(options, (res) => {
+        
+        res.on('data', (d) => {
+
+                process.stdout.write(d);
+        });
+    }).on('error', (e) => {
+        console.error(e);
+    });
+
+    console.log('first: ',  args.gender)
+    console.log('second: ' , args.race)
 }
